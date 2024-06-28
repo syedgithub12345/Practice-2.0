d65020815121cf5d718827fa73e76e4c7ee4bafd
Syed Safi <syedisaf@gmail.com>
2024-06-28 23:09:55 +0530

    sixth

---
+++ sumofn.java
@@ -0,0 +1,14 @@
+// ******************************************** */
+//sum of n numbers using recursion
+public class sumofn {
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
