9d3d5bbc11215c8d0535e5cc5729462327049237
Syed Safi <syedisaf@gmail.com>
2024-08-14 23:32:20 +0530

    twenty three

---
+++ hashmap.java
@@ -0,0 +1,25 @@
+//hashmap for check equal pair formed
+import java.util.*;
+
+public class hashmap {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            for (int freq : map.values()) {
+                if (freq % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        main main = new main();
+        int[] nums = { 3, 4, 2, 1, 43, 5, 9 };
+        System.out.println(main.divideArray(nums));
+    }
+}
