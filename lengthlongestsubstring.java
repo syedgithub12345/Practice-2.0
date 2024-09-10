ed1b4c5b39354614e7e9b9cfb6d031c821153efc
Syed Safi <syedisaf@gmail.com>
2024-09-10 23:32:25 +0530

    forty siz

---
+++ lengthlongestsubstring.java
@@ -0,0 +1,39 @@
+
+//longest substring
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }
+                set.add(s.charAt(j));
+                maxlength = Math.max(maxlength, j - i + 1);
+            }
+        }
+        return maxlength;
+    }
+}
+
+// import java.util.HashMap;
+
+// class Solution {
+// public int lengthOfLongestSubstring(String s) {
+// HashMap<Character, Integer> map = new HashMap<>();
+// int left = 0;
+// int maxLength = 0;
+
+// for (int right = 0; right < s.length(); right++) {
+// if (map.containsKey(s.charAt(right))) {
+// left = Math.max(map.get(s.charAt(right)) + 1, left);
+// }
+// map.put(s.charAt(right), right);
+// maxLength = Math.max(maxLength, right - left + 1);
+// }
+
+// return maxLength;
+// }
+// }
